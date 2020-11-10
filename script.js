6e37f6754a21fcff62e98718490d026388bf7b7b
marzieh67 <57456278+marzieh67@users.noreply.github.com>
2020-11-10 22:43:37 +0100

    search function is working

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //SELECT ELEMENTS
 ///const Element= document.querySelector(".className");
-const iconElement= document.querySelector(".weather-icon");
-const tempElement= document.querySelector(".temperature-value p");
-const descElement= document.querySelector(".temperature-description p");
-const locationElement= document.querySelector(".location p");
-const notificationElement= document.querySelector(".notification");
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const tempFeelsElement = document.querySelector(".temperature-feels-value p");
+const tempMinElement = document.querySelector(".temperature-min-value p");
+const tempMaxElement = document.querySelector(".temperature-max-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const humidityElement = document.querySelector(".humidity p");
 
 //App data
 
@@ -12,31 +15,44 @@ const weather = {};
 
 //GET WEATHER FROM API Provider
 const key = "c71eb334b9f16ce9ac215af9464c2b86";
-const lat = "51.51";
-const lon = "-0.13";
-getWeather();
+document.getElementById('citySearch').addEventListener('click', getWeather);
 
-function getWeather (lat, lon){
-    const api= `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${key}`;
+function getWeather() {
+    let cityName = document.getElementById("searchBar").value;
+    console.log(cityName);
+    const api = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric`;
     console.log(api);
-    fetch(api).then(function(response){
-       let data = response.json();
-       return data;
+    fetch(api).then(function (response) {
+        let data = response.json();
+        return data;
+        console.log(data);
     })
-    .then(function(data){
-        weather.temperature = Math.floor(data.main.temp);
-        weather.description = data.weather[0].description;
-        weather.iconId = data.weather[0].icon;
-        weather.city = data.name;
-        weather.country = data.sys.country;
-    })
-    .then(function(){
-        displayWeather();
-    })
+        .then(function (data) {
+            weather.temperature = Math.floor(data.main.temp);
+            weather.temperature_FeelsLike = Math.floor(data.main.feels_like);
+            weather.temperatureMin = Math.floor(data.main.temp_min);
+            weather.temperatureMax = Math.floor(data.main.temp_max);
+            weather.humidity = data.main.humidity;
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+            console.log(weather.humidity);
+            console.log(weather.description);
+        })
+        .then(function () {
+            displayWeather();
+        });
+
 }
-function displayWeather(){
+
+function displayWeather() {
     iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
-    tempElement.innerHTML = `${weather.temperature} &#176 <span> K</span>`;
+    tempElement.innerHTML = `${weather.temperature} &#176 <span> C</span>`;
+    tempFeelsElement.innerHTML = `${weather.temperature_FeelsLike} &#176 <span> C</span>`;
+    tempMinElement.innerHTML = `${weather.temperatureMin} &#176 <span> C</span>`;
+    tempMaxElement.innerHTML = `${weather.temperatureMax} &#176 <span> C</span>`;
+    humidityElement.innerHTML = `${weather.humidity} <span>&#37</span>`;
     descElement.innerHTML = weather.description;
     locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }
