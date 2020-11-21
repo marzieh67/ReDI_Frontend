704096bf418bd5d90e47de59a81b524ac91c58c6
marzieh67 <57456278+marzieh67@users.noreply.github.com>
2020-11-21 10:17:06 +0100

    problem in display predicted items

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 const currentDateElement = document.querySelector(".currentDate p");
 const humidityElement = document.querySelector(".humidity p");
+/*
 const temDay0Element = document.querySelector(".temperature-day0-value");
 const temDay1Element = document.querySelector(".temperature-day1-value");
 const temDay2Element = document.querySelector(".temperature-day2-value");
@@ -23,16 +24,21 @@ const iconDay3Element = document.querySelector(".weather-day3-icon");
 const iconDay4Element = document.querySelector(".weather-day4-icon");
 const iconDay5Element = document.querySelector(".weather-day5-icon");
 const iconDay6Element = document.querySelector(".weather-day6-icon");
+*/
+const tempPredMaxElement = document.querySelector(".tempPredMax");
+const tempPredMinElement = document.querySelector(".tempPredMin");
+const tempPredIconElement = document.querySelector(".weather-pred-icon");
+const dayElement = document.querySelector(".day");
+
 
 //App data
 
 const weather = {};
 var coordinate = [];
 
-
 //GET WEATHER FROM API Provider
 const key = "c71eb334b9f16ce9ac215af9464c2b86";
-document.getElementById('citySearch').addEventListener('click', getCoordinate);
+document.getElementById("citySearch").addEventListener("click", getCoordinate);
 
 function getCoordinate() {
     let cityName = document.getElementById("searchBar").value;
@@ -40,12 +46,12 @@ function getCoordinate() {
     const apiLoc = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric`;
 
     console.log(apiLoc);
-    fetch(apiLoc).then(function (response) {
-        let dataLoc = response.json();
-        return dataLoc;
-    })
+    fetch(apiLoc)
+        .then(function (response) {
+            let dataLoc = response.json();
+            return dataLoc;
+        })
         .then(function (dataLoc) {
-
             var lon = dataLoc.coord.lon;
             var lat = dataLoc.coord.lat;
             weather.city = dataLoc.name;
@@ -58,7 +64,6 @@ function getCoordinate() {
         .then(function (geocoordinate) {
             getWeather(geocoordinate);
         });
-
 }
 
 function getWeather(geocoordinate) {
@@ -67,16 +72,18 @@ function getWeather(geocoordinate) {
 
     const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${key}&units=metric`;
     console.log(api);
-    fetch(api).then(function (response) {
-        let data = response.json();
-        return data;
-    })
+    fetch(api)
+        .then(function (response) {
+            let data = response.json();
+            return data;
+        })
         .then(function (data) {
             weather.temperature = Math.floor(data.current.temp);
             weather.temperature_FeelsLike = Math.floor(data.current.feels_like);
             weather.humidity = data.current.humidity;
             weather.description = data.current.weather[0].description;
             weather.iconId = data.current.weather[0].icon;
+            /*
             weather.iconIdDay0 = data.daily[0].weather[0].icon;
             weather.iconIdDay1 = data.daily[1].weather[0].icon;
             weather.iconIdDay2 = data.daily[2].weather[0].icon;
@@ -84,6 +91,17 @@ function getWeather(geocoordinate) {
             weather.iconIdDay4 = data.daily[4].weather[0].icon;
             weather.iconIdDay5 = data.daily[5].weather[0].icon;
             weather.iconIdDay6 = data.daily[6].weather[0].icon;
+            dailyTemperature = [];
+*/
+
+            // dailyTemperature[i].className = "card-content";
+            //dailyTemperature[i].id = "tempPredValue";
+            //tempPredValueElement.appendChild(dailyTemperature[i]);
+            //weather.tempDay = data.daily[i].temp.day;
+            // console.log(weather.tempDay);
+            //tempPredValueElement.innerHTML = `${weather.tempDay}&#176 <span> C</span>`;
+
+/*
             weather.temDay0 = Math.floor(data.daily[0].temp.day);
             weather.temDay1 = Math.floor(data.daily[1].temp.day);
             weather.temDay2 = Math.floor(data.daily[2].temp.day);
@@ -91,46 +109,97 @@ function getWeather(geocoordinate) {
             weather.temDay4 = Math.floor(data.daily[4].temp.day);
             weather.temDay5 = Math.floor(data.daily[5].temp.day);
             weather.temDay6 = Math.floor(data.daily[6].temp.day);
+*/
             const unixTimestamp = data.current.dt;
             const timezoneOffset = data.timezone_offset;
             const milliseconds = (unixTimestamp + timezoneOffset) * 1000;
-            const dateObject = new Date(milliseconds)
-            weather.currentDate = dateObject.toLocaleString('en-GB', { timeZone: 'UTC' });
-            console.log(dateObject.getDay());
-            var day = "";
-            switch (dateObject.getDay()) {
-                case 0:
-                    day = "Sunday";
-                    break;
-                case 1:
-                    day = "Monday";
-                    break;
-                case 2:
-                    day = "Tuesday";
-                    break;
-                case 3:
-                    day = "Wednesday";
-                    break;
-                case 4:
-                    day = "Thursday";
-                    break;
-                case 5:
-                    day = "Friday";
-                    break;
-                case 6:
-                    day = "Saturday";
-            };
-
-            weather.dayOfWeek = day;
+            const currentDateObject = new Date(milliseconds);
+            weather.currentDate = currentDateObject.toLocaleString("en-GB", {
+                timeZone: "UTC",
+            });
+            var today = getDaysOfWeek(currentDateObject.getDay());
+            weather.dayOfWeek = today;
             console.log(weather.dayOfWeek);
+
+            for (var records = 0; records < data.daily.length; records++) {
+                var iconContainer = document.getElementById("predIcon");
+                var divIcon = document.createElement('div');
+                divIcon.classList.add("item");
+                weather.predIconId = data.daily[records].weather[0].icon;
+                divIcon.innerHTML = `<img src="images/icons/${weather.predIconId}.svg"/>`;
+                iconContainer.appendChild(document.createElement('p'));
+                iconContainer.appendChild(divIcon);
+
+
+                var tempMaxContainer = document.getElementById("predMaxTemp");
+                var divMaxTemp = document.createElement('div');
+                weather.predTempMax = Math.floor(data.daily[records].temp.max);
+                divMaxTemp.innerHTML = `${weather.predTempMax}&#176 <span> C &#8673</span>`;
+                tempMaxContainer.appendChild(document.createElement('p'));
+                tempMaxContainer.appendChild(divMaxTemp);
+
+                var tempMinContainer = document.getElementById("predMinTemp");
+                var divMinTemp = document.createElement('div');
+                weather.predTempMin = Math.floor(data.daily[records].temp.min);
+                divMinTemp.innerHTML = `${weather.predTempMin}&#176 <span> C &#8675</span>`;
+                tempMinContainer.appendChild(document.createElement('p'));
+                tempMinContainer.appendChild(divMinTemp);
+
+                var dayContainer = document.getElementById("predDay");
+                var divDay = document.createElement('div');
+                weather.dayOfWeekPred = getDaysOfWeek(((currentDateObject.getDay()) + records + 1) % 7);
+                console.log(getDaysOfWeek((currentDateObject.getDay() + records) % 7));
+                divDay.innerHTML = `${weather.dayOfWeekPred}`;
+                dayContainer.appendChild(document.createElement('p'));
+                dayContainer.appendChild(divDay);
+                /*
+                 // let predictedElement = document.getElementById("predictionElement");
+                //predictedElement.appendChild(document.createElement('p'));
+                //predictedElement.appendChild(divIcon);
+                predictedElement.innerHTML = `${weather.dayOfWeekPred}
+                                              <img src="icons/${weather.predIconId}.svg"/>
+                                              ${weather.predTempMax}&#176 <span> C &#8673</span>
+                                              ${weather.predTempMin}&#176 <span> C &#8675</span>`;
+                                              */
+
+            }
+
         })
         .then(function () {
             displayWeather();
         });
-
 }
+
+function getDaysOfWeek(x) {
+    switch (x) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+    }
+    return day;
+}
+
+
+
 function displayWeather() {
-    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    iconElement.innerHTML = `<img src="images/icons/${weather.iconId}.svg"/>`;
     tempElement.innerHTML = `${weather.temperature} &#176 <span> C</span>`;
     tempFeelsElement.innerHTML = `${weather.temperature_FeelsLike} &#176 <span> C</span>`;
     tempMinElement.innerHTML = `${weather.temperatureMin} &#176 <span> C</span>`;
@@ -139,6 +208,8 @@ function displayWeather() {
     descElement.innerHTML = weather.description;
     locationElement.innerHTML = `${weather.city}, ${weather.country}`;
     currentDateElement.innerHTML = `${weather.dayOfWeek}, ${weather.currentDate}`;
+
+    /*
     temDay0Element.innerHTML = `${weather.temDay0} &#176 <span> C</span>`;
     temDay1Element.innerHTML = `${weather.temDay1} &#176 <span> C</span>`;
     temDay2Element.innerHTML = `${weather.temDay2} &#176 <span> C</span>`;
@@ -152,6 +223,7 @@ function displayWeather() {
     iconDay3Element.innerHTML = `<img src="icons/${weather.iconIdDay3}.png"/>`;
     iconDay4Element.innerHTML = `<img src="icons/${weather.iconIdDay4}.png"/>`;
     iconDay5Element.innerHTML = `<img src="icons/${weather.iconIdDay5}.png"/>`;
-    iconDay6Element.innerHTML = `<img src="icons/${weather.iconIdDay6}.png"/>`;
-}
+    iconDay6Element.innerHTML = `<img src="icons/${weather.iconIdDay6}.png"/>`; */
+    //tempPredValue.innerHTML = weather.predTempDay;
 
+}
